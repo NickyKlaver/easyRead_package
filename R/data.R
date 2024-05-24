70961310ccead13bb320a25b62ddde1070205a08
NickyKlaver <nickyklaver50@gmail.com>
2024-05-24 13:30:04 +0200

    last package commit (I hope)

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Clean Data for the R Package
+#'
+#' A cleaned version of the raw dataset.
+#'
+#' @docType data
+#' @usage data(clean_R_package_data)
+#' @format A row of multiple heights in cm.
+#' @keywords datasets
+"clean_R_package_data"
--- data-raw/Normalize_raw_data.R
+++ data-raw/Normalize_raw_data.R
@@ -7,11 +7,11 @@
 library(dplyr)
 
 # Read the raw dataset
-raw_R_package_data <- read.csv2("./Raw_data/Random_data_R_package.csv", header = F)
+raw_R_package_data <- read.csv2("./data-raw/Random_data_R_package.csv", header = F)
 
 # Normalize the data
 clean_R_package_data <- raw_R_package_data %>%
   as.numeric(raw_R_package_data)
 
 # Save the data to the data folder
-save(clean_R_package_data, file = "data/Clean_data_R_package.RData")
+save(clean_R_package_data, file = "./data/clean_R_package_data.RData")
